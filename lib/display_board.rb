32d1bbc965b58bb04a957286beb709ce23757a58
Rielle Quiambao <rmq6@duke.edu>
2018-07-19 17:45:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def format_cell(cell)
-  row = " #{cell} "
-end
 
 def display_board(board)
-  format_cell(board[0])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  
 end
